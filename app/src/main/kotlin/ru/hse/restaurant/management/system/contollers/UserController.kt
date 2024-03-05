925641280a619f854e5ededd2c0d3b2fe02f5a3f
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-03-06 01:11:49 +0300

    Add mapstruct

--- app/src/main/kotlin/ru/hse/restaurant/management/system/contollers/UserController.kt
+++ app/src/main/kotlin/ru/hse/restaurant/management/system/contollers/UserController.kt
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.*
 import ru.hse.restaurant.management.system.data.entities.User
 import ru.hse.restaurant.management.system.data.repositories.UserRepository
 import ru.hse.restaurant.management.system.dto.DtoUser
-import ru.hse.restaurant.management.system.enums.Role
 import ru.hse.restaurant.management.system.mappers.UserDtoToUserDataEntityMapper
-import java.util.*
 
 @RestController
 @RequestMapping("/users")
