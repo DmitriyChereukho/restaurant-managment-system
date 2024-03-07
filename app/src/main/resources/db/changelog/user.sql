cf1652ae881e676769d9c2c9d98e887848a494ee
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2024-03-07 21:55:51 +0300

    Fix user storage

--- app/src/main/resources/db/changelog/user.sql
+++ app/src/main/resources/db/changelog/user.sql
@@ -3,7 +3,7 @@
 create table "users"(
     id uuid not null constraint users_pk primary key,
     username varchar(64) not null,
-    password varchar(64) not null,
+    password varchar(256) not null,
     phone_num varchar(64) not null,
     role varchar(16) not null
 )
